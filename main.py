8f562f600e5b5b151b9b0f5ca2c4b5de31b27ed5
A1eXeYM82 <malyginalex82@gmail.com>
2022-11-16 13:34:04 +0300

    add file

---
+++ main.py
@@ -0,0 +1,38 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import requests
+
+# print(4**2)
+# print(4//2)
+
+
+# num = int(input(' Введите число '))
+# if num % 2 == 0:
+#     print('четное число', num)
+# else:
+#     print('нечетное число')
+# letter = input('input text ')
+# if letter == 'a' or letter == 'e' or letter == 'i' or letter == 'o':
+#     print('гласная буква')
+# elif letter == 'y':
+#     print('согласная и гласная')
+# else:
+#     print('согласная')
+
+list1 = [3, 2, 8, 5, 10, 6]
+max_number = max(list1)
+print("Наибольшее число:", max_number)
---
+++ note.py
@@ -0,0 +1,33 @@
+a = 1
+print(type(a))
+
+a = 1
+b = 73
+c = -12
+d = 2
+
+e = a + b
+print(e)
+print(type(e))
+
+x = 0.25
+y = 1.45
+z = y - x
+print(z)
+print(type(z))
+print (0.1+0.1*5-0.3*4)
+
+
+a = -13
+b = 7
+
+a = a - b
+b = a + b
+a = b - a
+
+print(b)
+
+value = int(input())
+print(type(value))
+
+print(31 % 2 + 31 % 2)
